bf50415d29cd0d63d4f1343a63182dc56eb6c464
Faiz679 <daffa.makarim8@gmail.com>
2025-07-20 18:33:55 +0700

    Projects - IoT Insight
    Awalan card buat IoT Insight

--- iotwebfrontend/src/Projects.jsx
+++ iotwebfrontend/src/Projects.jsx
@@ -1,5 +1,6 @@
-import ProjectCard from "/components/ProjectCard.jsx";
-import ProjectCarousel from "/components/ProjectCarousel";
+import OurProjectsCard from "/src/components/OurProjectsCard.jsx";
+import OurProjectsCarousel from "/src/components/OurProjectsCarousel";
+import IotInsightCard from "/src/components/IotInsightCard";
 
 function Projects() {
   return (
@@ -7,43 +8,54 @@ function Projects() {
       <h1 className="flex items-center justify-center font-bold text-[50px] text-[var(--color-biru-tua)] mt-15 mb-5">
         Our Projects
       </h1>
-      <ProjectCarousel>
-        <ProjectCard
+
+      <OurProjectsCarousel>
+        <OurProjectsCard
           title="KSM IoT UPNVJ Website Development"
           description="This project focuses on the development of an official website for KSM IoT UPNVJ, aiming to serve as an informative and interactive platform for students, members, and the public."
           image="/src/assets/LOGO_IOT.png"
         />
-        <ProjectCard
+        <OurProjectsCard
           title="Smart Home Monitoring"
           description="Lorem ipsum dolor sit amet consectetur adipiscing elit. Quisque faucibus ex sapien vitae pellentesque sem placerat. In id cursus mi pretium tellus duis convallis. Tempus leo eu aenean sed diam urna tempor. Pulvinar vivamus fringilla lacus nec metus bibendum egestas. Iaculis massa nisl malesuada lacinia integer nunc posuere. Ut hendrerit semper vel class aptent taciti sociosqu. Ad litora torquent per conubia nostra inceptos himenaeos."
           image="/images/smart-home.png"
         />
-        <ProjectCard
+        <OurProjectsCard
           title="IoT Plant Watering System"
           description="An automatic plant watering solution using soil moisture sensors and solenoid valve control to optimize irrigation."
           image=""
         />
-        <ProjectCard
+        <OurProjectsCard
           title="IoT Plant Watering System"
           description="An automatic plant watering solution using soil moisture sensors and solenoid valve control to optimize irrigation."
           image="/images/plant-watering.png"
         />
-        <ProjectCard
+        <OurProjectsCard
           title="IoT Plant Watering System"
           description="An automatic plant watering solution using soil moisture sensors and solenoid valve control to optimize irrigation."
           image="/images/plant-watering.png"
         />
-        <ProjectCard
+        <OurProjectsCard
           title="IoT Plant Watering System"
           description="An automatic plant watering solution using soil moisture sensors and solenoid valve control to optimize irrigation."
           image="/images/plant-watering.png"
         />
-        <ProjectCard
+        <OurProjectsCard
           title="IoT Plant Watering System"
           description="An automatic plant watering solution using soil moisture sensors and solenoid valve control to optimize irrigation."
           image="/images/plant-watering.png"
         />
-      </ProjectCarousel>
+      </OurProjectsCarousel>
+
+      <h1 className="flex items-center justify-center font-bold text-[50px] text-[var(--color-biru-tua)] mt-15 mb-5">
+        IoT Insight
+      </h1>
+
+      <IotInsightCard
+        vol="1"
+        title="KSM IoT UPNVJ Website Development KSM IoT UPNVJ Website Development"
+        image="/src/assets/LOGO_IOT.png"
+      />
     </>
   );
 }
