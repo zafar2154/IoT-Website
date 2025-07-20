5433882db3da0ef8285b3b13d5611b2c81a66a6d
gusnisnadi <isnadygoesny@gmail.com>
2025-07-20 14:31:01 +0700

    nambahin footer + nyoba foto

--- iotwebfrontend/src/kepengurusan.jsx
+++ iotwebfrontend/src/kepengurusan.jsx
@@ -1,13 +1,13 @@
+import Anggotakepengurusan from "./AnggotaKepengurusan"
+import Footerkepengurusan from "./Footerkepengurusan"
+
 function Kepengurusan() {
     return (
         <div className='h-dvh justify-items-center '>
             <p className='font-optima font-[600] 
                             text-[50px]'>Our Team</p>
-            <div className='w-dvw h-[65px] bg-black opacity-30 mb-[4rem]'>1</div>
-            <div className='w-dvw h-[65px] bg-black opacity-30 mb-[4rem]'>2</div>
-            <div className='w-dvw h-[65px] bg-black opacity-30 mb-[4rem]'>3</div>
-            <div className='w-dvw h-[65px] bg-black opacity-30 mb-[4rem]'>4</div>
-            <div className='w-dvw h-[65px] bg-black opacity-30 mb-[4rem]'>5</div>
+            <Anggotakepengurusan />
+            <Footerkepengurusan />
         </div>
     )
 }
