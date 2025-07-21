3a5814de99e3f6e9da80793f6b70fcac2bf9eae0
IoTUPNVJ <internetofthings.upnvj@gmail.com>
2025-07-21 10:41:34 +0700

    nambahin deskripsi contact

--- iotwebfrontend/src/App.jsx
+++ iotwebfrontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Nav from './components/nav';
+import { Contact } from './contact';
 
 function App() {
   return (
     <>
       <Nav />
+      <Contact />
     </>
   );
 }
