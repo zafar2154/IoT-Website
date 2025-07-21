61be359fb1cc7a3c5425aa5a85ecccfc7099e4a4
zafar2154 <zafar2154.ez@gmail.com>
2025-07-21 16:05:38 +0700

    Nambahin contact cuman sampai form
    sisanya belum, masih ada bug di foto kanan, border onclick

--- iotwebfrontend/src/Footerkepengurusan.jsx
+++ iotwebfrontend/src/Footerkepengurusan.jsx
@@ -9,13 +9,13 @@ function Footerkepengurusan() {
                 <a href="" className='z-10 font-optima font-[300] text-[20px]'>contact info</a>
             </div>
             <div className='flex flex-col w-[400px] h-[400px] items-center'>
-                <img src="public/Logo_IoT.png" alt="Logo_IoT" className='w-[180px] h-[201px]'></img>
+                <img src="Logo_IoT.png" alt="Logo_IoT" className='w-[180px] h-[201px]'></img>
                 <p className='font-optima text-[24px] font-[700]'>KSM Internet Of Things</p>
                 <p className='font-optima text-[16px] font-[400] mb-[10px]'>UPN "Veteran" Jakarta</p>
-                <img src="public/slogan.png" alt="Slogan" className='w-[368.5px] h-[77px]'></img>
+                <img src="slogan.png" alt="Slogan" className='w-[368.5px] h-[77px]'></img>
                 <div className='flex w-[400px] h-[100px] justify-center gap-[10px]'>
-                    <img src="public/Linkedin_Footer.png" alt="linkedin" className='w-[57px] h-[57px]'></img>
-                    <img src="public/Ig_Footer.png" alt="ig" className='w-[57px] h-[57px]'></img>
+                    <img src="Linkedin_Footer.png" alt="linkedin" className='w-[57px] h-[57px]'></img>
+                    <img src="Ig_Footer.png" alt="ig" className='w-[57px] h-[57px]'></img>
                 </div>
             </div>
             <div className='pt-[80px] pr-[80px] flex flex-col gap-[10px] w-[300px] h-[300px] items-end'>
