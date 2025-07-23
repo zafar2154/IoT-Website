5a5264587689c328ab816c4fe5fd99ba5ffe3188
rmdhnptr22 <ramadhaniyuditama30295@gmail.com>
2025-07-24 00:36:36 +0700

    membuat tampilan awal, our vision/mision, dan Footer

---
+++ iotwebfrontend/src/components/Footer.jsx
@@ -0,0 +1,31 @@
+function Footerkepengurusan() {
+
+    return (
+        <div className='flex w-full pl-[100px] pr-[100px] pt-[100px] h-[700px] bg-gradient-to-b from-white via-biru-footer-kepengurusan to-biru-muda
+                        justify-between'>
+            <div className='pt-[80px] flex flex-col gap-[10px] w-[300px] h-[300px] items-end '>
+                <a href="" className='z-10 font-optima font-[900] text-[35px] text-biru-tua'>Homebase</a>
+                <a href="" className='z-10 font-optima font-[300] text-[20px] text-biru-tua'>alamat</a>
+            </div>
+            <div className='flex flex-col w-[400px] h-[400px] items-center'>
+                <img src="public/Logo_IoT.png" alt="Logo_IoT" className='w-[180px] h-[201px]'></img>
+                <p className='font-titillium text-[24px] font-[700]'>KSM Internet Of Things</p>
+                <p className='font-titillium text-[16px] font-[400] mb-[10px]'>UPN "Veteran" Jakarta</p>
+                <div className='flex w-[400px] h-[100px] justify-center gap-[10px]'>
+                    <img src="public/Linkedin_Footer.png" alt="linkedin" className='w-[57px] h-[57px]'></img>
+                    <img src="public/Ig_Footer.png" alt="ig" className='w-[57px] h-[57px]'></img>
+                </div>
+            </div>
+            <div className='pt-[80px] pr-[80px] flex flex-col gap-[10px] w-[300px] h-[300px] items-end'>
+                <img src="public/slogan.png" alt="Slogan" className='w-[368.5px] h-[77px]'></img>
+                <a href="" className='z-10 font-optima font-[900] text-[35px] text-biru-tua'>Quick Links</a>
+                <a href="" className='z-10 font-optima font-normal text-[20px] text-biru-tua'>Contact Us</a>
+                <a href="" className='z-10 font-optima font-[300] text-[20px] text-biru-tua'>About Us</a>
+                <a href="" className='z-10 font-optima font-[300] text-[20px] text-biru-tua'>Iot Insight</a>
+                <a href="" className='z-10 font-optima font-[300] text-[20px] text-biru-tua'>Article</a>
+            </div>
+        </div>
+    )
+}
+
+export default Footerkepengurusan
