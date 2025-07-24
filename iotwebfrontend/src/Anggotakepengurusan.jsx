9cc5c4a188c0726fd53fff762d2c0e83af24ddb4
gusnisnadi <isnadygoesny@gmail.com>
2025-07-24 21:12:32 +0700

    masukin foto dan link

--- iotwebfrontend/src/Anggotakepengurusan.jsx
+++ iotwebfrontend/src/Anggotakepengurusan.jsx
@@ -1,34 +1,75 @@
 function Anggotakepengurusan() {
+
     return(
         <>
-            <div className='flex w-full h-[655px] bg-[rgba(0,0,0,0.3)] mb-[4rem] z-0 justify-between items-center'>
+            <div className='flex w-full h-[655px] bg-[rgba(0,0,0,0.3)] mb-[4rem] z-0 justify-between items-center overflow-hidden'>
                 <div className='absolute w-[100%] p-[20px] h-[100%] flex justify-between items-center'>
                     <a href="" className=''>
                         <img src="public/Chevron_left.png" alt="tombolKiri" className=''></img>
                     </a>
                     <a href="" className=''>
-                        <img src="public/Chevron_right.png" alt="tombolkanan" className=''></img>
+                        <img src="/Chevron_right.png" alt="tombolkanan" className=''></img>
                     </a>
                 </div>
                 <div className='perspective-[825px] origin-left scale-x-[0.9] overflow-hidden'>
-                    <div className='flex flex-col w-[472px] h-[515px] bg-abu-muda rounded-[15px] z-10 justify-between items-center perspective-distant rotate-y-[45deg] transform-gpu origin-right duration-[1s] ease-in-out scale-x-[0.9] scale-y-[0.60]'>
-
+                    <div className='overflow-hidden flex flex-col w-[472px] h-[515px] bg-abu-muda rounded-[15px] z-10 justify-between items-center perspective-distant rotate-y-[45deg] transform-gpu origin-right duration-[1s] ease-in-out scale-x-[0.9] scale-y-[0.60]'>
+                        <a href="https://www.instagram.com/p/DMNGCGvx6oq/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==">
+                            <img src="/FotKep/chairman.png" alt="chairman" className="w-full h-full"></img>
+                        </a>
                     </div>   
                 </div>
-                <div className='relative w-[472px] h-[515px] bg-biru-muda rounded-[15px] z-10 overflow-hidden'>
-                    <div className='absolute w-[100%] h-[100%] justify-between items-center flex flex-col pr-[180px] '>
-                        <div className='w-[72px] h-[100px] bg-abu-muda opacity-100 z-20'></div>
-                        <div className='w-[72px] h-[100px] bg-abu-muda opacity-100 z-20'></div>
-                    </div>   
+                <div className='relative w-[472px] h-[515px] bg-biru-muda rounded-[15px] justify-center items-center z-10 overflow-hidden'>
+                    <a href="https://www.instagram.com/p/DMNFxi8RXV2/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==">
+                        <img src="/FotKep/academic advisor.png" alt="academicadvisor" className="w-full h-full"></img> 
+                    </a>  
                 </div>
                 <div className='perspective-[825px] origin-right scale-x-[0.9]'>
-                    <div className='flex flex-col w-[472px] h-[515px] bg-biru-sedang rounded-[15px] z-10 justify-between items-center perspective-distant rotate-y-[-45deg] transform-gpu origin-left duration-[1s] ease-in-out scale-x-[0.9] scale-y-[0.60]'></div>   
+                    <div className='overflow-hidden flex flex-col w-[472px] h-[515px] bg-biru-sedang rounded-[15px] z-10 justify-between items-center perspective-distant rotate-y-[-45deg] transform-gpu origin-left duration-[1s] ease-in-out scale-x-[0.9] scale-y-[0.60]'>
+                        <a href="https://www.instagram.com/p/DMNFizGxH8n/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==">
+                            <img src="/FotKep/vice chairman.png" alt="vice chairman" className="w-full h-full"></img>
+                        </a>    
+                    </div>   
                 </div> 
             </div>
-            <div className='w-full h-[65px] bg-[rgba(0,0,0,0.3)] mb-[4rem]'>2</div>
-            <div className='w-full h-[65px] bg-[rgba(0,0,0,0.3)] mb-[4rem]'>3</div>
-            <div className='w-full h-[65px] bg-[rgba(0,0,0,0.3)] mb-[4rem]'>4</div>
-            <div className='w-full h-[65px] bg-[rgba(0,0,0,0.3)] mb-[4rem]'>5</div>
+            <div className='flex w-full h-[655px] bg-[rgba(0,0,0,0.3)] mb-[4rem] justify-center items-center'>
+                <a href="https://www.instagram.com/p/DMNDGnBx7J8/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" className='relative w-[472px] h-[515px] bg-biru-muda rounded-[15px] z-10 overflow-hidden'>
+                    <img src="/FotKep/Firmware.png" alt="firmware" className="w-full h-full"></img>   
+                </a>
+                <a href="https://www.instagram.com/p/DMND9y7xSG8/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" className='relative w-[472px] h-[515px] bg-biru-muda rounded-[15px]  z-10 overflow-hidden'>
+                    <img src="/FotKep/hardware.png" alt="hardware" className="w-full h-full"></img>   
+                </a>
+                <a href="https://www.instagram.com/p/DMNDqnqR9Ca/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" className='relative w-[472px] h-[515px] bg-biru-muda rounded-[15px]  z-10 overflow-hidden'>
+                    <img src="/FotKep/engineer.png" alt="engineer" className="w-full h-full"></img>   
+                </a>
+                <a href="https://www.instagram.com/p/DMNCVUMRsUZ/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" className='relative w-[472px] h-[515px] bg-biru-muda rounded-[15px]  z-10 overflow-hidden'>
+                    <img src="/FotKep/network.png" alt="network" className="w-full h-full"></img>   
+                </a>
+                <a href="https://www.instagram.com/p/DMNDV7ZRhBy/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" className='relative w-[472px] h-[515px] bg-biru-muda rounded-[15px]  z-10 overflow-hidden'>
+                    <img src="/FotKep/Software.png" alt="software" className="w-full h-full"></img>   
+                </a>
+                <a href="https://www.instagram.com/p/DMNCzpNxAZn/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" className='relative w-[472px] h-[515px] bg-biru-muda rounded-[15px]  z-10 overflow-hidden'>
+                    <img src="/FotKep/UX.png" alt="UX" className="w-full h-full"></img>   
+                </a>
+            </div>
+            <div className='flex w-full h-[655px] bg-[rgba(0,0,0,0.3)] mb-[4rem] justify-center items-center'>
+                <a href="https://www.instagram.com/p/DMNEqw_Rkwu/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" className='relative w-[472px] h-[515px] bg-biru-muda rounded-[15px]  z-10 overflow-hidden'>
+                    <img src="/FotKep/hrd.png" alt="hrd" className="w-full h-full"></img>   
+                </a>
+                <a href="https://www.instagram.com/p/DMNEOzGxpno/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" className='relative w-[472px] h-[515px] bg-biru-muda rounded-[15px]  z-10 overflow-hidden'>
+                    <img src="/FotKep/akademik.png" alt="akademik" className="w-full h-full"></img>   
+                </a>
+            </div>
+            <div className='flex w-full h-[655px] bg-[rgba(0,0,0,0.3)] mb-[4rem] justify-center items-center'>
+                <a href="https://www.instagram.com/p/DMM_1IpxBPU/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" className='relative w-[472px] h-[515px] bg-biru-muda rounded-[15px]  z-10 overflow-hidden'>
+                    <img src="/FotKep/humas.png" alt="humas" className="w-full h-full"></img>   
+                </a>
+                <a href="https://www.instagram.com/p/DMNAZGpxtCN/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" className='relative w-[472px] h-[515px] bg-biru-muda rounded-[15px]  z-10 overflow-hidden'>
+                    <img src="/FotKep/medfo.png" alt="medfo" className="w-full h-full"></img>   
+                </a>
+                <a href="https://www.instagram.com/p/DMNB-72R3MD/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==" className='relative w-[472px] h-[515px] bg-biru-muda rounded-[15px]  z-10 overflow-hidden'>
+                    <img src="/FotKep/Medkraf.png" alt="medkraf" className="w-full h-full"></img>   
+                </a>
+            </div>
         </>
     )
 }
