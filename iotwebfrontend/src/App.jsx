5f74967551ec2b8052c9acd27febba8ab4fc2ba9
Faiz679 <daffa.makarim8@gmail.com>
2025-07-25 16:45:29 +0700

    update footer
    update efek icon footer, ngebuat utils buat rotating icon dari 2 x 4 sisi persegi, ganti logo icon yg HD, ngerapihin footer

--- iotwebfrontend/src/App.jsx
+++ iotwebfrontend/src/App.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import Nav from './components/nav';
+import React from "react";
+import Nav from "./components/nav";
+import Footerkepengurusan from "./components/Footerkepengurusan";
 
 function App() {
   return (
     <>
       <Nav />
+      <Footerkepengurusan />
     </>
   );
 }
