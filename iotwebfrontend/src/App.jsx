1617f98c60dc371766b28252a106ce4153972573
IoTUPNVJ <internetofthings.upnvj@gmail.com>
2025-07-31 09:44:49 +0700

    first commit

--- iotwebfrontend/src/App.jsx
+++ iotwebfrontend/src/App.jsx
@@ -1,23 +1,11 @@
 import React from 'react';
 import Nav from './components/nav';
-import Home from './components/welcome';
-import VisionMission from './components/visimisi';
-import OurProgram from './components/ourprogram';
-import OurAchievement from './components/ourachievement';
-import MoreInsight from './components/insight';
-import Footerkepengurusan from './components/Footer';
 
 
 function App() {
   return (
     <>
       <Nav/>
-      <Home />
-      <VisionMission />
-      <OurProgram />
-      <OurAchievement />
-      <MoreInsight />
-      <Footerkepengurusan />
     </>
   );
 }
