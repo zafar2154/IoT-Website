4be2ddb5c64b1f28cd75c6c2a6c6ea21afb0de52
zafar2154 <zafar2154.ez@gmail.com>
2025-08-06 22:59:26 +0700

    fix bug map, gambar media card ilang

--- iotwebfrontend/src/App.jsx
+++ iotwebfrontend/src/App.jsx
@@ -1,17 +1,10 @@
 import Nav from './components/nav';
 import { Contact } from './contact';
 import Footer from './components/footer';
-import { useEffect } from 'react';
-import Bg from './utils/Bg'
+import Bg from './utils/Bg';
 function App() {
-  useEffect(() => {
-    setTimeout(() => {
-      window.scrollTo({ top: 0, behavior: 'instant' });
-    }, 300);
-  }, []);
-
   return (
-    <div className='relative'>
+    <div className="relative">
       <Bg />
       <Nav />
       <Contact />
