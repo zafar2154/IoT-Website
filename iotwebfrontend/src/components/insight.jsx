d7f4b1b88455127a34838b0c3cbc5609e3752691
rmdhnptr22 <ramadhaniyuditama30295@gmail.com>
2025-08-07 14:34:11 +0700

    menambahkan gradiensi yang berbeda di setiap tabel

--- iotwebfrontend/src/components/insight.jsx
+++ iotwebfrontend/src/components/insight.jsx
@@ -35,13 +35,12 @@ const MoreInsight = () => {
                 className="absolute top-0 left-0 w-full h-full bg-cover bg-center transition-transform duration-700 ease-in-out scale-100 group-hover:scale-110"
                 style={{
                   backgroundImage: `url(${division.image})`,
-                  filter: isActive ? "brightness(0.8)" : "brightness(0.4)",
                 }}
               />
 
               {/* Vertical Title */}
               <div
-                className={`absolute z-10 font-extrabold text-white transition-all duration-700 ease-in-out ${
+                className={`absolute z-10 font-extrabold text-black transition-all duration-700 ease-in-out ${
                   isActive
                     ? "text-3xl bottom-[180px] left-6 scale-100 text-left rotate-0"
                     : "text-5xl top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 -rotate-90"
@@ -56,9 +55,30 @@ const MoreInsight = () => {
                 {division.name}
               </div>
 
+              {!isActive && (
+                <div
+                  className={`absolute w-full h-full inset-0 bg-gradient-to-b to-color pointer-events-none transform transition-all duration-700 ease-in-out
+                    ${isActive? "opacity-0 -translate-x-10" : "opacity-100 translate-x-0"} ${
+                  index === 0 ? "from-gray-200/60 to-gray-600/70" :
+                  index === 1 ? "from-purple-400/60 to-yellow-400/60" :
+                  index === 2 ? "from-yellow-300/60 to-pink-500/80" :
+                  index === 3 ? "from-blue-300/70 to-blue-300/70" :
+                  index === 4 ? "from-green-300/70 to-green-700/70" :
+                  "from-gray-400/90"
+              }`}
+                style={{
+                  top: "0%",
+                  left: "50%",
+                  transform: "translateX(-50%)",
+                  zIndex: 1,
+                }}
+                />
+              )}
+
+              
               {/* Description on Hover */}
               {isActive && (
-                <div className="relative z-20 p-4 text-white text-sm font-medium mt-auto bg-black/40 backdrop-blur-sm rounded-t-2xl">
+                <div className="relative z-20 p-4 text-white text-sm font-medium mt-auto bg-black/10 backdrop-blur-sm rounded-t-2xl">
                   <p className="mb-4">{lorem}</p>
                   <button className="px-4 py-2 bg-white text-black rounded-lg text-sm font-semibold hover:bg-gray-200 transition">
                     Learn More
