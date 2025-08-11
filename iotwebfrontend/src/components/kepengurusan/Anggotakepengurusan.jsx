de405d8c2451f1ccd3a4f30377fb162700d78415
gusnisnadi <isnadygoesny@gmail.com>
2025-08-11 11:14:15 +0700

    update footer + nav 2

--- iotwebfrontend/src/components/kepengurusan/Anggotakepengurusan.jsx
+++ iotwebfrontend/src/components/kepengurusan/Anggotakepengurusan.jsx
@@ -9,7 +9,6 @@ function Anggotakepengurusan() {
     return(
         <>
             <div className='relative flex w-full h-auto max-h-[270px] sm:max-h-[340px] md:max-h-[450px] lg:max-h-[510px] xl:max-h-[665px] duration-500 transition-all md:mb-[4rem] mb-[2rem] z-0 justify-center items-center'>
-                
                 <div className='absolute w-[90%] h-full bg-[rgba(0,0,0,0.3)] rounded-[25px] left-1/2 -translate-x-1/2'></div>
                 <ImageSliderBPH />                    
             </div>
