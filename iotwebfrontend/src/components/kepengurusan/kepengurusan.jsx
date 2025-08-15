3b5f84abec4f2ab60d23903cae7e384d76a6b549
gusnisnadi <isnadygoesny@gmail.com>
2025-08-15 13:52:11 +0700

    fade in

--- iotwebfrontend/src/components/kepengurusan/kepengurusan.jsx
+++ iotwebfrontend/src/components/kepengurusan/kepengurusan.jsx
@@ -1,14 +1,16 @@
 import Anggotakepengurusan from "./Anggotakepengurusan"
 import Bg from "../../utils/Bg"
 import Footer from "../footer"
+import FadeIn from "../../utils/fadeIn"
 
 function Kepengurusan() {
     return (
         <div className='relative overflow-x-hidden'>
             <Bg />
             <div className='relatve h-full w-full justify-items-center'>
-                <p className='font-optima font-bold 
-                                text-[30px] m-[35px] md:m-[65px] md:text-[50px] duration-1000 transition-all text-biru-tua'>Our Team</p>
+                <FadeIn delay={1.6} direction={'down'}>
+                    <p className='font-optima font-bold text-[30px] m-[35px] md:m-[65px] md:text-[50px] duration-1000 transition-all text-biru-tua'>Our Team</p>
+                </FadeIn>
                 <Anggotakepengurusan />
                 <Footer />
             </div>
