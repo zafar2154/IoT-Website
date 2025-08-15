d2530469ce7f7eb5d2b38d48592bd5ebb2f2fc83
zafar2154 <zafar2154.ez@gmail.com>
2025-08-15 15:51:37 +0700

    nambahin router

--- iotwebfrontend/src/App.jsx
+++ iotwebfrontend/src/App.jsx
@@ -1,14 +1,32 @@
 import Nav from './components/nav';
 import { Contact } from './contact';
-import Footer from './components/Footer';
+import Footer from './components/footer';
+import { useEffect } from 'react';
 import Bg from './utils/Bg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 function App() {
+  useEffect(() => {
+    setTimeout(() => {
+      window.scrollTo({ top: 0, behavior: 'instant' });
+    }, 300);
+  }, []);
+
   return (
     <div className="relative">
-      <Bg />
-      <Nav />
-      <Contact />
-      <Footer />
+      <Router>
+        <Bg />
+        <Nav />
+        <Routes>
+          <Route path="/" element={<div>Home Page</div>} />
+          <Route path="/home" element={<div>Home Page</div>} />
+          <Route path="/aboutus" element={<div>About Us Page</div>} />
+          <Route path="/kepengurusan" element={<div>Kepengurusan Page</div>} />
+          <Route path="/project" element={<div>Project Page</div>} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
