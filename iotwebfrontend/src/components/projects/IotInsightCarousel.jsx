e2828303c683e94dcd6d777988a1df9c383bc952
Faiz679 <daffa.makarim8@gmail.com>
2025-08-22 07:04:57 +0700

    bug fixing

--- iotwebfrontend/src/components/projects/IotInsightCarousel.jsx
+++ iotwebfrontend/src/components/projects/IotInsightCarousel.jsx
@@ -1,36 +1,16 @@
-import React, { useEffect, useRef } from "react";
+import React, { useRef } from "react";
 import { Pagination, Mousewheel, Autoplay, Keyboard } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
+
 import "swiper/css";
+import "swiper/css/autoplay";
 import "swiper/css/pagination";
 
 export default function IotInsightCarousel({ children }) {
-  const containerRef = useRef(null);
   const swiperRef = useRef(null);
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        if (entry.isIntersecting) {
-          setTimeout(() => {
-            if (swiperRef.current) {
-              swiperRef.current.autoplay.start();
-            }
-          }, 0);
-          observer.disconnect();
-        }
-      },
-      { threshold: 0.2 }
-    );
-
-    if (containerRef.current) observer.observe(containerRef.current);
-
-    return () => observer.disconnect();
-  }, []);
 
   return (
     <div
-      ref={containerRef}
       className="iot-insight cursor-default select-none mx-4"
     >
       <Swiper
@@ -38,6 +18,20 @@ export default function IotInsightCarousel({ children }) {
         onSwiper={(swiper) => {
           swiperRef.current = swiper;
           swiper.autoplay.stop();
+
+          const observer = new IntersectionObserver(
+            ([entry]) => {
+              if (entry.isIntersecting) {
+                swiper.autoplay.start();
+                observer.disconnect(); // stop observing once started
+              }
+            },
+            { threshold: 0.2 },
+          );
+
+          if (swiper.el) {
+            observer.observe(swiper.el);
+          }
         }}
         slidesPerView={3}
         threshold={5}
@@ -52,7 +46,10 @@ export default function IotInsightCarousel({ children }) {
           dynamicMainBullets: 1,
         }}
         loop={true}
-        autoplay={{ delay: 3000, pauseOnMouseEnter: true }}
+        autoplay={{
+          delay: 3000,
+          pauseOnMouseEnter: true,
+        }}
         breakpoints={{
           // when window width is >= 1024px (desktop)
           1200: {
